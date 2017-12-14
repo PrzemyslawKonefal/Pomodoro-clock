4d53509e74685e7f3e83330d1b4039e9beb4a9d7
Przemkon <smeagol65@gmail.com>
2017-12-14 01:42:19 +0100

    clocks logic + their display

--- js.js
+++ js.js
@@ -5,18 +5,27 @@ function createInterval(){
   $(".clocks").css("display", "block");
   for(i=1; i<numberOfSessions; i++){
     $("<div class = 'workClock clock'><p class = 'min'>"+duration+"</p><p class ='sec'>00</p></div>").appendTo(".clocks");
-
     $("<div class = 'breakClock clock'><p class = 'min'>"+breakTime+"</p><p class ='sec'>00</p></div>").appendTo(".clocks");
   }
     $("<div class = 'workClock clock'><p class = 'min'>"+duration+"</p><p class ='sec'>00</p></div>").appendTo(".clocks");
-    $("<img src='close.png'>").appendTo(".clock");
-    activateClock();
+    $("<img class ='closeButton' src='close.png'>").appendTo(".clock");
+    $("<div class = 'StopGo'><img class ='play' src='play.png'></div>").appendTo(".clock");
+    activateClock(0);
 }
 
-$(".clocks").on('click', 'img', function(){
+$(".clocks").on('click', '.closeButton', function(){
   $(this).closest(".clock").hasClass("active")?isActive=true:isActive=false
     $(this).closest(".clock").remove();
-    if(isActive){activateClock();}
+    if(isActive){activateClock(0);}
+});
+$(".clocks").on('click', '.play', function(){
+  $(this).closest(".clock").hasClass("active")?isActive=true:isActive=false
+  if(isActive){
+    clearInterval(interval);
+    $(this).closest(".play").attr('src','play.png');
+    $(this).closest(".clock").removeClass('active');
+ }
+  else activateClock($(this).closest(".clock").index());
 });
 
 function getSettings(){
@@ -24,21 +33,24 @@ function getSettings(){
   breakTime = document.getElementById("breakTime").value;
   numberOfSessions = document.getElementById("numberOfSessions").value;
 }
-function activateClock(){
+
+function activateClock(clockNum){
     clearInterval(interval);
-    $(".clock").eq(0).css('background', 'green');
-    $(".clock").eq(0).addClass('active');
-    secs = 0;
-    if($(".clock").eq(0).hasClass("workClock")){mins = duration;}
-    else {mins = breakTime;}
-    interval = setInterval(function(){
-      secs--;
-      if(mins<=0 && secs <=0){clearInterval(interval); $(".clock").eq(0).remove(); activateClock();}
-        else if(secs <=0) {secs=59; mins--;}
-      if(secs<10 && secs.length<2) {secs = '0'+secs;}
-      if(mins<10 && mins.length<2) {mins = '0'+mins;}
-      $(".min").eq(0).html(mins);
-      $(".sec").eq(0).html(secs);
-}, 1000);
+    $(".clock").removeClass('active');
+    $(".play").attr('src', 'play.png')
+    $(".play").eq(clockNum).attr('src', 'pause.png')
+    $(".clock").eq(clockNum).addClass('active');
+    mins = $(".min").eq(clockNum).html();
+    secs = $(".sec").eq(clockNum).html();
+      interval = setInterval(function(){
+            secs--;
+            if(mins<=0 && secs <=0){ $(".clock").eq(clockNum).remove(); activateClock(clockNum);}
+              else if(secs <=0) {secs=59; mins--;}
+            if(mins<10)$(".min").eq(clockNum).html('0'+mins);
+              else $(".min").eq(clockNum).html(mins);
+            if(secs<10)$(".sec").eq(clockNum).html('0'+secs);
+              else $(".sec").eq(clockNum).html(secs);
+
+          }, 1000);
 
 }
